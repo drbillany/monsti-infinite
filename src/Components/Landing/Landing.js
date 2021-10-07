655e4331a03dc1d486f93670842291f0c2bb1c40
drbillany <90723951+drbillany@users.noreply.github.com>
2021-10-07 12:18:06 +0200

    added the images with the src folder

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -8,16 +8,14 @@ import TwitterIcon from "@material-ui/icons/Twitter";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import TelegramIcon from "@material-ui/icons/Telegram";
 import TextsmsIcon from "@material-ui/icons/Textsms";
-import discordIcon from "../../images/discord.svg";
-import telegramIcon from "../../images/telegram.svg";
-import twitterIcon from "../../images/twitter.svg";
-import facebookIcon from "../../images/facebook.svg";
-import youtubeIcon from "../../images/youtube.svg";
+import playtoGreen from "../../images/DozaMonsta-Banner-Green.png";
+import playtoRed from "../../images/DozaMonsta-Banner-Red.png";
+import playtoBanner from "../../images/DozaMonsta-Banner.png";
+
 export default function Landing() {
   const [offsetY, setOffsetY] = useState(0);
   const handleScroll = () => setOffsetY(window.pageYOffset);
 
- 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
@@ -26,16 +24,20 @@ export default function Landing() {
   return (
     <>
       <div className={L.page}>
-        <Nav/>
+        <Nav />
         <section className={L.preview}>
           <div className={L.imgHolder}></div>
           <img
-            style={{ transform: `translate(-50%, -${offsetY * 1.5}px  )` }}
             className={L.previewImage}
-            src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo.png"
-            srcset="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo.png 857w, https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo-300x140.png 300w, https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo-768x358.png 768w"
-            alt="MonstaInfinite"
+            src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo.png'
+            srcset='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo.png 857w, https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo-300x140.png 300w, https://monstainfinite.com/wp-content/uploads/2021/08/monsta-logo-768x358.png 768w'
+            alt='MonstaInfinite'
           />
+          <div className={`${L.previewImage} ${L.previewImagePlayto}`}>
+            <img src={playtoGreen} alt='playtogreen' />
+            <img src={playtoRed} alt='playtogreen' />
+            <img src={playtoBanner} alt='playtogreen' />
+          </div>
         </section>
         <section className={L.parallax}>
           <img
@@ -43,26 +45,27 @@ export default function Landing() {
               transform: `translate(-50%, -${offsetY / 80}%  )`,
               width: `calc(10% + ${offsetY}px)`,
             }}
-            src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-explosion-1024x1005.png"
-            alt=""
+            src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-explosion-1024x1005.png'
+            alt=''
           />
           <img
             style={{
               transform: `translate(-50%, -${offsetY / 50}%  )`,
               width: `calc(100% )`,
             }}
-            src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-flare-flip-724x1024.png"
-            alt=""
+            src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-flare-flip-724x1024.png'
+            alt=''
           />
           <img
             style={{
               transform: `translate(-50%, -${offsetY / 50}%  )`,
               width: `calc(10% + ${offsetY * 0.5}px)`,
             }}
-            src="https://monstainfinite.com/wp-content/uploads/2021/08/castle-blur-mega-crop-min.png"
-            alt=""
+            src='https://monstainfinite.com/wp-content/uploads/2021/08/castle-blur-mega-crop-min.png'
+            alt=''
           />
         </section>
+
         <section className={L.potions}>
           <div className={L.textContainer}>
             <p className={L.text1}>
@@ -90,15 +93,15 @@ export default function Landing() {
         <section className={L.wooden}>
           <img
             className={L.emerald}
-            src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-td-gem-1024x1024.png"
-            alt=""
+            src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-td-gem-1024x1024.png'
+            alt=''
           />
           <div className={L.about}>
             <div className={L.mobileImgHolder}>
               <img
                 className={L.mobileImg}
-                src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-td-gameplay@2X-593x1024.png"
-                alt=""
+                src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-td-gameplay@2X-593x1024.png'
+                alt=''
               />
             </div>
             <div className={L.aboutGame}>
@@ -130,23 +133,23 @@ export default function Landing() {
           <div className={L.linkHolders}>
             <div className={L.socials}>
               <YouTubeIcon />
-              <a href="youtube">YouTube</a>
+              <a href='youtube'>YouTube</a>
             </div>
             <div className={L.socials}>
               <FacebookIcon />
-              <a href="facebook">Facebook</a>
+              <a href='facebook'>Facebook</a>
             </div>
             <div className={L.socials}>
               <TwitterIcon />
-              <a href="Twitter">Twitter</a>
+              <a href='Twitter'>Twitter</a>
             </div>
             <div className={L.socials}>
               <TextsmsIcon />
-              <a href="discord">Discord</a>
+              <a href='discord'>Discord</a>
             </div>
             <div className={L.socials}>
               <TelegramIcon />
-              <a href="telegram">Telegram</a>
+              <a href='telegram'>Telegram</a>
             </div>
           </div>
         </section>
@@ -155,8 +158,8 @@ export default function Landing() {
           <div className={L.flexChild}>
             <img
               className={L.flexImg}
-              src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-arena-features-survival-1024x1024.png"
-              alt=""
+              src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-arena-features-survival-1024x1024.png'
+              alt=''
             />
             <p className={L.title}>Survival</p>
             <p className={L.desc}>
@@ -167,8 +170,8 @@ export default function Landing() {
           <div className={L.flexChild}>
             <img
               className={L.flexImg}
-              src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-arena-features-war-1024x1024.png"
-              alt=""
+              src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-arena-features-war-1024x1024.png'
+              alt=''
             />
             <p className={L.title}>Monsta War</p>
             <p className={L.desc}>
@@ -179,8 +182,8 @@ export default function Landing() {
           <div className={L.flexChild}>
             <img
               className={L.flexImg}
-              src="https://monstainfinite.com/wp-content/uploads/2021/08/monsta-arena-features-breeding-1024x1024.png"
-              alt=""
+              src='https://monstainfinite.com/wp-content/uploads/2021/08/monsta-arena-features-breeding-1024x1024.png'
+              alt=''
             />
             <p className={L.title}>Synthesis Cloning</p>
             <p className={L.desc}>
